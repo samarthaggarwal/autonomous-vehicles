9a5a8197fbdb98c439ede287bb89224614d72e7d
akash17mittal <akashmittal1798@gmail.com>
2022-12-06 23:28:19 -0600

    simulator and real vehicle separated

--- project/lidar/config.py
+++ project/lidar/config.py
@@ -1,11 +1,5 @@
-is_real_mode = True
-if is_real_mode:
-    resolution = 0.2  # 1 pixel is how many meters
-    side_range = (-20, 20)
-    fwd_range = (2.5, 8)
-    height_range = (-1.3, 1.5)
-else:
-    resolution = 0.2  # 1 pixel is how many meters
-    side_range = (-20, 20)
-    fwd_range = (2.0, 20)
-    height_range = (-1.0, 0.5)
+is_real_mode = True # False
+resolution = 0.2  # 1 pixel is how many meters
+side_range = (-20, 20)
+fwd_range = (2.5, 8)
+height_range = (-1.3, 1.5)
--- project/lidar/detect_obstacles.py
+++ project/lidar/detect_obstacles.py
@@ -3,11 +3,12 @@ from lidarProcessing import LidarProcessing
 from process_birds_eye_view import ProcessBirdsEyeView
 from config import *
 
+
 def run_lidar():
     # init rospy node
     rospy.init_node("lidar_obstacle_map_generation")
     lidarProcessor = LidarProcessing(resolution=resolution, side_range=side_range, fwd_range=fwd_range,
-                                     height_range=height_range)
+                                     height_range=height_range, is_real_mode=is_real_mode)
     birdsEyeViewProcessor = ProcessBirdsEyeView(resolution=resolution)
 
     rate = rospy.Rate(3)  # 100 Hz
@@ -18,6 +19,7 @@ def run_lidar():
         # update birds eye view
         lidarProcessor.publish_birds_eye_view()
     rospy.spin()
+
 
 if __name__ == "__main__":
     run_lidar()
