b153777f52fae902f3bc1f3fdc50df1cd352fd81
akash17mittal <akashmittal1798@gmail.com>
2022-12-15 12:29:51 -0600

    orientation change

--- project/summon_vehicle.py
+++ project/summon_vehicle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#================================================================
+# ================================================================
 # File name: gem_gnss_pp_tracker_pid.py                                                                  
 # Description: gnss waypoints tracker using pid and pure pursuit                                                                
 # Author: Hang Cui
@@ -10,12 +10,12 @@
 # Version: 0.1                                                                    
 # Usage: rosrun gem_gnss gem_gnss_pp_tracker.py                                                                      
 # Python version: 3.8                                                             
-#================================================================
+# ================================================================
 
 from __future__ import print_function
 
 # Python Headers
-import os 
+import os
 import csv
 import math
 import numpy as np
@@ -23,7 +23,7 @@ from numpy import linalg as la
 import scipy.signal as signal
 
 # ROS Headers
-import alvinxy.alvinxy as axy # Import AlvinXY transformation module
+import alvinxy.alvinxy as axy  # Import AlvinXY transformation module
 import rospy
 
 # GEM Sensor Headers
@@ -39,6 +39,8 @@ from cv_bridge import CvBridge
 from lidar.util import transform_to_image_coordinates, transform_to_taped_coordinates
 from lidar.config import resolution
 import cv2
+from smoothpath import bezier_curve
+
 
 def transform_points_from_image_to_tape(points, resolution):
     path = []
@@ -50,21 +52,22 @@ def transform_points_from_image_to_tape(points, resolution):
     path = np.array(path)
     return path
 
+
 class PID(object):
 
     def __init__(self, kp, ki, kd, wg=None):
 
-        self.iterm  = 0
+        self.iterm = 0
         self.last_t = None
         self.last_e = 0
-        self.kp     = kp
-        self.ki     = ki
-        self.kd     = kd
-        self.wg     = wg
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.wg = wg
         self.derror = 0
 
     def reset(self):
-        self.iterm  = 0
+        self.iterm = 0
         self.last_e = 0
         self.last_t = None
 
@@ -98,7 +101,6 @@ class PID(object):
 class OnlineFilter(object):
 
     def __init__(self, cutoff, fs, order):
-        
         nyq = 0.5 * fs
         normal_cutoff = cutoff / nyq
 
@@ -107,49 +109,45 @@ class OnlineFilter(object):
 
         # Initialize
         self.z = signal.lfilter_zi(self.b, self.a)
-    
+
     def get_data(self, data):
         filted, self.z = signal.lfilter(self.b, self.a, [data], zi=self.z)
         return filted
 
 
 class PurePursuit(object):
-    
+
     def __init__(self):
 
-        self.rate       = rospy.Rate(10)
+        self.rate = rospy.Rate(10)
 
         self.look_ahead = 4
-        self.far_look_ahead = 10  # meters
-        self.wheelbase  = 1.75 # meters
-        self.offset     = 0.46 # meters
+        self.far_look_ahead = 8  # meters
+        self.wheelbase = 1.75  # meters
+        self.offset = 0.46  # meters
 
-        self.gnss_sub   = rospy.Subscriber("/novatel/inspva", Inspva, self.inspva_callback)
+        self.gnss_sub = rospy.Subscriber("/novatel/inspva", Inspva, self.inspva_callback)
 
-        self.lat        = 0.0
-        self.lon        = 0.0
-        self.heading    = 0.0
+        self.lat = 0.0
+        self.lon = 0.0
+        self.heading = 0.0
 
         self.enable_sub = rospy.Subscriber("/pacmod/as_tx/enable", Bool, self.enable_callback)
 
-        self.speed_sub  = rospy.Subscriber("/pacmod/as_tx/vehicle_speed", Float64, self.speed_callback)
-        self.speed      = 0.0
+        self.speed_sub = rospy.Subscriber("/pacmod/as_tx/vehicle_speed", Float64, self.speed_callback)
+        self.speed = 0.0
 
-        self.olat       = 40.0928563
-        self.olon       = -88.2359994
-
-        # read waypoints into the system 
-        self.goal       = 0            
-        # self.read_waypoints() 
+        self.olat = 40.0928563
+        self.olon = -88.2359994
 
         self.desired_speed = 0.75  # m/s, reference speed
-        self.max_accel     = 0.4 # % of acceleration
-        self.pid_speed     = PID(1.2, 0.2, 0.6, wg=20)
-        self.speed_filter  = OnlineFilter(1.2, 30, 4)
+        self.max_accel = 0.4  # % of acceleration
+        self.pid_speed = PID(1.2, 0.2, 0.6, wg=20)
+        self.speed_filter = OnlineFilter(1.2, 30, 4)
 
         # -------------------- PACMod setup --------------------
 
-        self.gem_enable    = False
+        self.gem_enable = False
         self.pacmod_enable = False
 
         # GEM vehicle enable, publish once
@@ -160,32 +158,32 @@ class PurePursuit(object):
         # GEM vehicle gear control, neutral, forward and reverse, publish once
         self.gear_pub = rospy.Publisher('/pacmod/as_rx/shift_cmd', PacmodCmd, queue_size=1)
         self.gear_cmd = PacmodCmd()
-        self.gear_cmd.ui16_cmd = 2 # SHIFT_NEUTRAL
+        self.gear_cmd.ui16_cmd = 2  # SHIFT_NEUTRAL
 
         # GEM vehilce brake control
         self.brake_pub = rospy.Publisher('/pacmod/as_rx/brake_cmd', PacmodCmd, queue_size=1)
         self.brake_cmd = PacmodCmd()
         self.brake_cmd.enable = False
-        self.brake_cmd.clear  = True
+        self.brake_cmd.clear = True
         self.brake_cmd.ignore = True
 
         # GEM vechile forward motion control
         self.accel_pub = rospy.Publisher('/pacmod/as_rx/accel_cmd', PacmodCmd, queue_size=1)
         self.accel_cmd = PacmodCmd()
         self.accel_cmd.enable = False
-        self.accel_cmd.clear  = True
+        self.accel_cmd.clear = True
         self.accel_cmd.ignore = True
 
         # GEM vechile turn signal control
         self.turn_pub = rospy.Publisher('/pacmod/as_rx/turn_cmd', PacmodCmd, queue_size=1)
         self.turn_cmd = PacmodCmd()
-        self.turn_cmd.ui16_cmd = 1 # None
+        self.turn_cmd.ui16_cmd = 1  # None
 
         # GEM vechile steering wheel control
         self.steer_pub = rospy.Publisher('/pacmod/as_rx/steer_cmd', PositionWithSpeed, queue_size=1)
         self.steer_cmd = PositionWithSpeed()
-        self.steer_cmd.angular_position = 0.0 # radians, -: clockwise, +: counter-clockwise
-        self.steer_cmd.angular_velocity_limit = 2.0 # radians/second
+        self.steer_cmd.angular_position = 0.0  # radians, -: clockwise, +: counter-clockwise
+        self.steer_cmd.angular_velocity_limit = 2.0  # radians/second
 
         # path planner subscriptions
         # subscribe to obstacle map from Lidar
@@ -197,20 +195,21 @@ class PurePursuit(object):
 
         self.destinationTapeCoordinates = (50.0, -10.0)  # in metric coordinate system with taped start as origin
         # convert to image coordinate system
-        self.destinationImageCoordinates = transform_to_image_coordinates(self.destinationTapeCoordinates[0], self.destinationTapeCoordinates[1],
+        self.destinationImageCoordinates = transform_to_image_coordinates(self.destinationTapeCoordinates[0],
+                                                                          self.destinationTapeCoordinates[1],
                                                                           resolution)
 
     def plot_path(self, path, obstacleMapRGB):
         """
         plots path on RVIZ
         """
-        if self.obstacleMap is None or len(path)<2:
+        if self.obstacleMap is None or len(path) < 2:
             return
         # obstacleMapRGB = cv2.cvtColor(self.obstacleMap * 255, cv2.COLOR_GRAY2RGB)
         color = (0, 255, 0)
-        for i in range(len(path)-1):
-            obstacleMapRGB = cv2.line(obstacleMapRGB, path[i][::-1], path[i+1][::-1], color, 5)
-            obstacleMapRGB = cv2.circle(obstacleMapRGB, path[i+1][::-1], 10, (0, 255, 255), -1)
+        for i in range(len(path) - 1):
+            obstacleMapRGB = cv2.line(obstacleMapRGB, path[i][::-1], path[i + 1][::-1], color, 5)
+            obstacleMapRGB = cv2.circle(obstacleMapRGB, path[i + 1][::-1], 10, (0, 255, 255), -1)
 
         obstacleMapRGB = cv2.circle(obstacleMapRGB, path[0][::-1], 10, (255, 0, 0), -1)
         obstacleMapRGB = cv2.circle(obstacleMapRGB, path[-1][::-1], 10, (0, 0, 255), -1)
@@ -222,9 +221,9 @@ class PurePursuit(object):
         self.voronoiMapPub.publish(obstacleMapRGB)
 
     def inspva_callback(self, inspva_msg):
-        self.lat     = inspva_msg.latitude  # latitude
-        self.lon     = inspva_msg.longitude # longitude
-        self.heading = inspva_msg.azimuth   # heading in degrees
+        self.lat = inspva_msg.latitude  # latitude
+        self.lon = inspva_msg.longitude  # longitude
+        self.heading = inspva_msg.azimuth  # heading in degrees
 
     def updateObstacleMap(self, data):
         """
@@ -234,7 +233,7 @@ class PurePursuit(object):
         # print(self.obstacleMap)
 
     def speed_callback(self, msg):
-        self.speed = round(msg.data, 3) # forward velocity in m/s
+        self.speed = round(msg.data, 3)  # forward velocity in m/s
 
     def enable_callback(self, msg):
         self.pacmod_enable = msg.data
@@ -246,9 +245,9 @@ class PurePursuit(object):
         # 270 <= heading < 360 --- 180 to 90   (pi to pi/2)
         if (heading_curr >= 0 and heading_curr < 90):
             yaw_curr = np.radians(90 - heading_curr)
-        elif(heading_curr >= 90 and heading_curr < 180):
+        elif (heading_curr >= 90 and heading_curr < 180):
             yaw_curr = np.radians(90 - heading_curr)
-        elif(heading_curr >= 180 and heading_curr < 270):
+        elif (heading_curr >= 180 and heading_curr < 270):
             yaw_curr = np.radians(90 - heading_curr)
         else:
             yaw_curr = np.radians(450 - heading_curr)
@@ -256,18 +255,18 @@ class PurePursuit(object):
 
     def front2steer(self, f_angle):
 
-        if(f_angle > 35):
+        if (f_angle > 35):
             f_angle = 35
 
         if (f_angle < -35):
             f_angle = -35
 
         if (f_angle > 0):
-            steer_angle = round(-0.1084*f_angle**2 + 21.775*f_angle, 2)
+            steer_angle = round(-0.1084 * f_angle ** 2 + 21.775 * f_angle, 2)
 
         elif (f_angle < 0):
             f_angle = -f_angle
-            steer_angle = -round(-0.1084*f_angle**2 + 21.775*f_angle, 2)
+            steer_angle = -round(-0.1084 * f_angle ** 2 + 21.775 * f_angle, 2)
         else:
             steer_angle = 0.0
 
@@ -276,7 +275,7 @@ class PurePursuit(object):
     def wps_to_local_xy(self, lon_wp, lat_wp):
         # convert GNSS waypoints into local fixed frame reprented in x and y
         lon_wp_x, lat_wp_y = axy.ll2xy(lat_wp, lon_wp, self.olat, self.olon)
-        return lon_wp_x, lat_wp_y   
+        return lon_wp_x, lat_wp_y
 
     def get_gem_state(self):
 
@@ -287,7 +286,7 @@ class PurePursuit(object):
 
         # heading to yaw (degrees to radians)
         # heading is calculated from two GNSS antennas
-        curr_yaw = self.heading_to_yaw(self.heading) 
+        curr_yaw = self.heading_to_yaw(self.heading)
 
         # reference point is located at the center of rear axle
         curr_x = local_x_curr - self.offset * np.cos(curr_yaw)
@@ -307,28 +306,27 @@ class PurePursuit(object):
         return round(np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2), 3)
 
     def start_pp(self):
-        
+
         while not rospy.is_shutdown():
 
             if (self.gem_enable == False):
 
-                if(self.pacmod_enable == True):
-
+                if (self.pacmod_enable == True):
                     # ---------- enable PACMod ----------
 
                     # enable forward gear
                     self.gear_cmd.ui16_cmd = 3
 
                     # enable brake
-                    self.brake_cmd.enable  = True
-                    self.brake_cmd.clear   = False
-                    self.brake_cmd.ignore  = False
+                    self.brake_cmd.enable = True
+                    self.brake_cmd.clear = False
+                    self.brake_cmd.ignore = False
                     self.brake_cmd.f64_cmd = 0.0
 
                     # enable gas 
-                    self.accel_cmd.enable  = True
-                    self.accel_cmd.clear   = False
-                    self.accel_cmd.ignore  = False
+                    self.accel_cmd.enable = True
+                    self.accel_cmd.clear = False
+                    self.accel_cmd.ignore = False
                     self.accel_cmd.f64_cmd = 0.0
 
                     self.gear_pub.publish(self.gear_cmd)
@@ -336,7 +334,7 @@ class PurePursuit(object):
 
                     self.turn_pub.publish(self.turn_cmd)
                     print("Turn Signal Ready!")
-                    
+
                     self.brake_pub.publish(self.brake_cmd)
                     print("Brake Engaged!")
 
@@ -344,8 +342,6 @@ class PurePursuit(object):
                     print("Gas Engaged!")
 
                     self.gem_enable = True
-
-
 
             curr_x, curr_y, curr_yaw = self.get_gem_state()
 
@@ -365,7 +361,7 @@ class PurePursuit(object):
                 bezierPathImageCoordinates = bezier_curve(np.array(path), 1000, voronoi, resolution)
                 self.plot_path(bezierPathImageCoordinates, voronoi.plot_regions())
                 bezierPathTapeCoordinates = transform_points_from_image_to_tape(bezierPathImageCoordinates,
-                                                                                          resolution)
+                                                                                resolution)
             except Exception as e:
                 print(f"src = {currLocationImageCoordinates} destination = {self.destinationImageCoordinates}")
                 print(str(e))
@@ -400,19 +396,19 @@ class PurePursuit(object):
             print(f"goal: {self.goal}, distance_from_next_chase_point:{distance_from_next_chase_point}")
 
             distance_from_destination = self.dist(self.destinationTapeCoordinates, (curr_x, curr_y))
-            
-            def orientation_ray_from_vehicle(point):
-                nextChasePointXTape, nextChasePointYTape = bezierPathTapeCoordinates[point][0],bezierPathTapeCoordinates[point][1]
 
+            def orientation_ray_from_vehicle(point):
+                nextChasePointXTape, nextChasePointYTape = bezierPathTapeCoordinates[point][0], \
+                    bezierPathTapeCoordinates[point][1]
                 # transforming the goal point into the vehicle coordinate frame
                 gvcx = nextChasePointXTape - curr_x
                 gvcy = nextChasePointYTape - curr_y
-                orientation = np.arctan2(gvcy, gvcx)
+                orientation_degrees = ((np.arctan2(gvcx, gvcy) * 180 / np.pi) + 360.0) % 360.0
+                orientation = self.heading_to_yaw(orientation_degrees)
                 return orientation
-            
+
             orientationOfLineConnectingNextChasePoint = orientation_ray_from_vehicle(goal)
             orientationOfLineConnectingFarLookAheadPoint = orientation_ray_from_vehicle(far_ahead_point)
-
 
             """
             # finding the distance of each way point from the current position
@@ -436,33 +432,33 @@ class PurePursuit(object):
             # true look-ahead distance between a waypoint and current position
             L = self.dist_arr[self.goal]
             """
-            
+
             # find the curvature and the angle
             alpha = orientationOfLineConnectingNextChasePoint - curr_yaw
             alpha_far = orientationOfLineConnectingFarLookAheadPoint - curr_yaw
-            
+
             # ----------------- tuning this part as needed -----------------
-            k       = 0.41
-            angle_i = math.atan((k * 2 * self.wheelbase * math.sin(alpha)) / L)
-            angle   = angle_i*2
+            k = 0.41
+            angle_i = math.atan((k * 2 * self.wheelbase * math.sin(alpha)) / distance_from_next_chase_point)
+            angle = angle_i * 2
             # ----------------- tuning this part as needed -----------------
-            
+
             f_delta = round(np.clip(angle, -0.61, 0.61), 3)
-            
+
             f_delta_deg = np.degrees(f_delta)
-            
+
             # steering_angle in degrees
             steering_angle = self.front2steer(f_delta_deg)
-            
-            if(self.gem_enable == True):
+
+            if (self.gem_enable == True):
                 print("Current index: " + str(self.goal))
                 print("Forward velocity: " + str(self.speed))
-                ct_error = round(np.sin(alpha) * L, 3)
+                ct_error = round(np.sin(alpha) * distance_from_next_chase_point, 3)
                 print("Crosstrack Error: " + str(ct_error))
                 print("Front steering angle: " + str(np.degrees(f_delta)) + " degrees")
-                print("Steering wheel angle: " + str(steering_angle) + " degrees" )
+                print("Steering wheel angle: " + str(steering_angle) + " degrees")
                 print("\n")
-            
+
             # implement constant pure pursuit controller
             if distance_from_destination < 4.0:
                 self.brake_cmd.f64_cmd = 1.0
@@ -477,26 +473,24 @@ class PurePursuit(object):
             #     desired_speed = 2.8
             else:
                 desired_speed = self.desired_speed
-            
+
             current_time = rospy.get_time()
-            filt_vel     = self.speed_filter.get_data(self.speed)
+            filt_vel = self.speed_filter.get_data(self.speed)
             output_accel = self.pid_speed.get_control(current_time, desired_speed - filt_vel)
-            
+
             if output_accel > self.max_accel:
                 output_accel = self.max_accel
-            
+
             if output_accel < 0.3:
                 output_accel = 0.3
-            
-            if (f_delta_deg <= 30 and f_delta_deg >= -30):
+
+            if f_delta_deg <= 30 and f_delta_deg >= -30:
                 self.turn_cmd.ui16_cmd = 1
-            elif(f_delta_deg > 30):
-                self.turn_cmd.ui16_cmd = 2 # turn left
+            elif f_delta_deg > 30:
+                self.turn_cmd.ui16_cmd = 2  # turn left
             else:
-                self.turn_cmd.ui16_cmd = 0 # turn right
-            
-            
-            
+                self.turn_cmd.ui16_cmd = 0  # turn right
+
             self.accel_cmd.f64_cmd = output_accel
             self.steer_cmd.angular_position = np.radians(steering_angle)
             self.accel_pub.publish(self.accel_cmd)
@@ -507,7 +501,6 @@ class PurePursuit(object):
 
 
 def pure_pursuit():
-
     rospy.init_node('gnss_pp_node', anonymous=True)
     pp = PurePursuit()
 
@@ -519,5 +512,3 @@ def pure_pursuit():
 
 if __name__ == '__main__':
     pure_pursuit()
-
-
